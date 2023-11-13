c951968cc626d29375ca6248cc85c76563e10c01
asmrajuwan <rajuwan.rj@gmail.com>
2023-11-13 18:27:47 +0600

    create and add OAuth component

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { SignIn, SignUp } from '../controllers/authController.js';
+import { SignIn, SignUp, google } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post("/sign-up",SignUp)
 router.post("/sign-in",SignIn)
+router.post('/google', google);
 
 export default router;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,71 +1,70 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
 import {
   signInFailure,
   signInStart,
-  signInSuccess
-} from '../redux/user/userSlice';
+  signInSuccess,
+} from "../redux/user/userSlice";
+
 
 const SignIn = () => {
-    const [formData,setFormData] = useState({});
+    const [formData, setFormData] = useState({});
     // const [error, setError] = useState(null);
     // const [loading, setLoading] = useState(false);
-    const {loading,error} = useSelector((state) => state.user)
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+    const { loading, error } = useSelector((state) => state.user);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-    const handleChange = (e)=>{
+    const handleChange = (e) => {
         setFormData({
             ...formData,
-            [e.target.id]: e.target.value
-        })
-
+            [e.target.id]: e.target.value,
+        });
     };
-    const handleSubmit =async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             // setLoading(true);
-            dispatch(signInStart())
-            const res = await fetch('/api/auth/sign-in', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(formData),
+            dispatch(signInStart());
+            const res = await fetch("/api/auth/sign-in", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
             });
             const data = await res.json();
             console.log(data);
             if (data.success === false) {
-              dispatch(signInFailure(data.message))
-              // setLoading(false);
-              // setError(data.message);
-              return;
+                dispatch(signInFailure(data.message));
+                // setLoading(false);
+                // setError(data.message);
+                return;
             }
-            dispatch(signInSuccess(data))
+            dispatch(signInSuccess(data));
             // setLoading(false);
             // setError(null);
-            navigate('/')
-            
-          } catch (error) {
-            dispatch(signInFailure(error.message))
+            navigate("/");
+        } catch (error) {
+            dispatch(signInFailure(error.message));
             // setLoading(false);
             // setError(error.message);
-          }
-    }
-        console.log(formData)
+        }
+    };
+    console.log(formData);
     return (
         <div className="mx-auto p-3 max-w-lg">
             <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
             <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-                
                 <input
                     type="email"
                     placeholder="email"
                     className="border p-3 rounded-lg"
                     id="email"
                     onChange={handleChange}
-                    />
+                />
                 <input
                     type="password"
                     placeholder="password"
@@ -73,9 +72,13 @@ const SignIn = () => {
                     id="password"
                     onChange={handleChange}
                 />
-                <button disabled={loading} className="bg-slate-700 uppercase hover:opacity-95 disabled:opacity-80 text-white p-3 rounded-lg" >
-                    {loading? 'loading...' : 'Sign In'}
+                <button
+                    disabled={loading}
+                    className="bg-slate-700 uppercase hover:opacity-95 disabled:opacity-80 text-white p-3 rounded-lg"
+                >
+                    {loading ? "loading..." : "Sign In"}
                 </button>
+                <OAuth/>
             </form>
             <div className="flex gap-2 mt-5">
                 <p>Do not Have an account? go to </p>
@@ -83,7 +86,7 @@ const SignIn = () => {
                     <span className="text-blue-700 ">Sign Up</span>
                 </Link>
             </div>
-            {error && <p className='text-red-500 mt-5'>{error}</p>}
+            {error && <p className="text-red-500 mt-5">{error}</p>}
         </div>
     );
 };
