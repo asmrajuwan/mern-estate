dad473dded3c7586c64ec9c1674e77de31744685
asmrajuwan <rajuwan.rj@gmail.com>
2023-11-16 23:22:11 +0600

    add delete user functionality

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { userUpdateController } from '../controllers/userController.js';
+import { userDeleteController, userUpdateController } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/update/:id',verifyToken, userUpdateController
+)
+router.delete('/delete/:id',verifyToken, userDeleteController
 )
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,9 @@ import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { app } from "../firebase";
 import {
+    deleteUserFailure,
+    deleteUserStart,
+    deleteUserSuccess,
     updateUserFailure,
     updateUserStart,
     updateUserSuccess,
@@ -83,6 +86,23 @@ export default function Profile() {
         }
     };
 
+    const handleDeleteUser = async () => {
+        try {
+          dispatch(deleteUserStart());
+          const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+            method: 'DELETE',
+          });
+          const data = await res.json();
+          if (data.success === false) {
+            dispatch(deleteUserFailure(data.message));
+            return;
+          }
+          dispatch(deleteUserSuccess(data));
+        } catch (error) {
+          dispatch(deleteUserFailure(error.message));
+        }
+      };
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -147,7 +167,7 @@ export default function Profile() {
             </form>
 
             <div className="flex justify-between mt-5">
-                <span className="text-red-700 cursor-pointer">
+                <span className="text-red-700 cursor-pointer"  onClick={handleDeleteUser}>
                     Delete account
                 </span>
                 <span className="text-red-700 cursor-pointer">Sign out</span>
