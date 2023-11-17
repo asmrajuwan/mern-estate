90ef0c3371cc138d12871bbc319150b3f6d54944
asmrajuwan <rajuwan.rj@gmail.com>
2023-11-17 12:37:21 +0600

    add create listing api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -3,6 +3,6 @@ import { createListingController } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
 
-router.post('/create',verifyToken, createListingController)
+router.post("/create",verifyToken, createListingController)
 
 export default router;
