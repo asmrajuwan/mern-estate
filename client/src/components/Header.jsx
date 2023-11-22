da3129cd4b1653e2fed261e2a002a6b5da19c1c6
asmrajuwan <rajuwan.rj@gmail.com>
2023-11-22 17:57:40 +0600

    update header search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Header = () => {
     const { currentUser } = useSelector((state) => state.user);
+    const [serachTerm,setSearchTerm] = useState('')
+    const navigate = useNavigate();
     
     
     const handleSubmit = (e) => {
       e.preventDefault();
-    
+      const urlParams = new URLSearchParams(window.location.search);
+      urlParams.set('searchTerm', serachTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`);
     };
+
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setSearchTerm(searchTermFromUrl);
+      }
+    }, [location.search]);
   
    
     return (
@@ -30,6 +44,8 @@ const Header = () => {
               type='text'
               placeholder='Search...'
               className='bg-transparent focus:outline-none w-24 sm:w-64'
+              value={serachTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
               
               
             />
