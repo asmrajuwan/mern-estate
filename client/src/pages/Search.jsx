059dde5c15b19e637da0b2205892803f1b97ea19
asmrajuwan <rajuwan.rj@gmail.com>
2023-11-23 12:27:01 +0600

    create listing item components

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ListingItem from "../components/ListingItem";
+import ListingItem from "../components/ListingItems";
 
 const Search = () => {
     const navigate = useNavigate();
