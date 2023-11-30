4dbb04c90ce81132f938615b51290699a4f9fcbc
asmrajuwan <rajuwan.rj@gmail.com>
2023-11-30 10:46:01 +0600

    fix pakage.json

--- api/index.js
+++ api/index.js
@@ -1,54 +1,36 @@
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import connectDB from './config/db.js';
-import authRouter from './routes/authRoute.js';
-import listingRouter from './routes/listingRoute.js';
-import userRouter from './routes/userRoute.js';
+import dotenv from "dotenv";
+import express from "express";
+import authRouter from "./routes/authRoute.js";
+import listingRouter from "./routes/listingRoute.js";
+import userRouter from "./routes/userRoute.js";
 
+import connectDB from './config/db.js';
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 connectDB();
 
 const app = express();
-
-// Middleware
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
 
-// Serve static files
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-// Catch-all route for SPA (Single Page Application) to handle client-side routing
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000!')
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal server error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/listing", listingRouter)
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.use((err,req,res,next) =>{
+    const statusCode =err.statusCode || 500;
+    const message = err.message || 'internal server error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
 });
