8a6f7f4a545e99faacac730e864f0f22d438b58f
asmrajuwan <rajuwan.rj@gmail.com>
2023-12-03 16:37:35 +0600

    changes index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import dotenv from "dotenv";
 import express from "express";
+import path from 'path';
 import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingRoute.js";
 import userRouter from "./routes/userRoute.js";
@@ -23,6 +24,12 @@ app.listen(3000,()=>{
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 app.use((err,req,res,next) =>{
