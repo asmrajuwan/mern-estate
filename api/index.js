8404eecf4257cf282e47d552cea9bc89935deae7
asmrajuwan <rajuwan.rj@gmail.com>
2023-12-09 01:08:20 +0600

    fix .env file

--- api/index.js
+++ api/index.js
@@ -20,8 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!')
+app.listen(process.env.SERVER_PORT,()=>{
+    console.log(`Server is running on port ${process.env.SERVER_PORT}!`)
 });
 
 app.use("/api/user", userRouter)
